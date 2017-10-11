c8811943ec50c41ba4c133101e07ad0a6edabbce
林志伟 <linzhiwei1@4399inc.com>
2017-10-11 10:08:39 +0800

    1. change method modifier

---
+++ buildSrc/src/main/groovy/com/cantalou/gradle/divider/tasks/CountMethodTask.java
@@ -0,0 +1,18 @@
+package com.cantalou.gradle.divider.tasks;
+
+import org.gradle.api.DefaultTask;
+
+/**
+ * Project Name: multidex<p>
+ * File Name:    CountMethodTask.java<p>
+ * ClassName:    CountMethodTask<p>
+ * <p>
+ * TODO.
+ *
+ * @author LinZhiWei
+ * @date 2017年09月26日 10:06
+ * <p>
+ * Copyright (c) 2017年, 4399 Network CO.ltd. All Rights Reserved.
+ */
+public class CountMethodTask extends DefaultTask {
+}
